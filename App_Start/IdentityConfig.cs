a405596d4341445ca69e239a2ec6372154209e35
ThienPhuong24 <tranthaithienphuong514@gmail.com>
2024-10-16 15:05:37 +0700

    Lưu các thay đổi cục bộ trước khi merge

--- App_Start/IdentityConfig.cs
+++ App_Start/IdentityConfig.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.Entity;
 using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
@@ -11,26 +14,91 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using WebsiteBanDoAnVaThucUong.Models;
+using System.Configuration;
 
 namespace WebsiteBanDoAnVaThucUong
 {
     public class EmailService : IIdentityMessageService
     {
+       
+
         public Task SendAsync(IdentityMessage message)
         {
             // Plug in your email service here to send an email.
             return Task.FromResult(0);
+            //return Task.Factory.StartNew(() =>
+            //{
+            //    sendMail(message);
+            //});
+        }
+        //void sendMail(IdentityMessage message)
+        //{
+        //    #region formatter
+        //    string text = string.Format("Please click on this link to {0}: {1}", message.Subject, message.Body);
+        //    string html = "Please confirm your account by clicking this link: <a href=\"" + message.Body + "\">link</a><br/>";
+        //    html += HttpUtility.HtmlEncode(@"Or copy and paste the following link into the browser: " + message.Body);
+        //    #endregion
+
+        //    // Khởi tạo đối tượng MailMessage
+        //    MailMessage msg = new MailMessage();
+        //    msg.From = new MailAddress(ConfigurationManager.AppSettings["Email"].ToString());
+        //    msg.To.Add(new MailAddress(message.Destination));
+        //    msg.Subject = message.Subject;
+        //    msg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain));
+        //    msg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html));
+
+        //    // Thiết lập SMTP client
+        //    SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587); // Port 587 cho TLS
+        //    smtpClient.EnableSsl = true;  // Kích hoạt SSL (TLS)
+
+        //    // Lấy email và password từ AppSettings
+        //    string email = ConfigurationManager.AppSettings["Email"].ToString();
+        //    string password = ConfigurationManager.AppSettings["Password"].ToString();
+
+        //    // Khởi tạo NetworkCredential với email và mật khẩu
+        //    System.Net.NetworkCredential credentials = new System.Net.NetworkCredential(email, password);
+        //    smtpClient.Credentials = credentials;
+
+        //    // Đặt thời gian chờ để tránh bị lỗi timeout
+        //    smtpClient.Timeout = 20000;  // 20 giây
+
+        //    try
+        //    {
+        //        // Gửi email
+        //        smtpClient.Send(msg);
+        //    }
+        //    catch (SmtpException ex)
+        //    {
+        //        // Ghi log hoặc xử lý ngoại lệ
+        //        throw new Exception("SMTP Error: " + ex.Message);
+        //    }
+        //}
+
+        void sendMail(IdentityMessage message)
+        {
+            #region formatter
+            string text = string.Format("Please click on this link to {0}: {1}", message.Subject, message.Body);
+            string html = "Please confirm your account by clicking this link: <a href=\"" + message.Body + "\">link</a><br/>";
+
+            html += HttpUtility.HtmlEncode(@"Or click on the copy the following link on the browser:" + message.Body);
+            #endregion
+
+            MailMessage msg = new MailMessage();
+            msg.From = new MailAddress(ConfigurationManager.AppSettings["Email"].ToString());
+            msg.To.Add(new MailAddress(message.Destination));
+            msg.Subject = message.Subject;
+            msg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain));
+            msg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html));
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", Convert.ToInt32(587));
+            System.Net.NetworkCredential credentials = new System.Net.NetworkCredential(ConfigurationManager.AppSettings["Email"].ToString(), ConfigurationManager.AppSettings["Password"].ToString());
+            smtpClient.Credentials = credentials;
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(msg);
         }
     }
 
-    public class SmsService : IIdentityMessageService
-    {
-        public Task SendAsync(IdentityMessage message)
-        {
-            // Plug in your SMS service here to send a text message.
-            return Task.FromResult(0);
-        }
-    }
+
 
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
     public class ApplicationUserManager : UserManager<ApplicationUser>
@@ -77,7 +145,7 @@ namespace WebsiteBanDoAnVaThucUong
                 BodyFormat = "Your security code is {0}"
             });
             manager.EmailService = new EmailService();
-            manager.SmsService = new SmsService();
+           // manager.SmsService = new SmsService();
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
             {
