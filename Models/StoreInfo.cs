5d7930f1e8d3b9c69ed5be5170336e40da5dee60
ngocdtm <ngocdtm@gmail.com>
2024-10-22 20:43:06 +0700

    soldout product

---
+++ Models/StoreInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteBanDoAnVaThucUong.Models
+{
+    public class StoreInfo
+    {
+        public int StockCount { get; set; }
+        public bool IsSoldOut { get; set; }
+    }
+}
